af5365fae315362be18e4afb5e7f1c4f77fcc30e
yeonoo <ywoo818@gmail.com>
2014-12-18 19:56:56 +0900

    주석 추가

--- src/main/java/dataForCalculate.java
+++ src/main/java/dataForCalculate.java
@@ -1,20 +1,18 @@
-package dday_calculator;
-
 public class dataForCalculate {
 	private int numberOfDaysOnYear;
 	private int[] numberOfDaysOnMonth, hourOnOneDay;
 
 	public dataForCalculate() {
 		numberOfDaysOnMonth = new int[] { 0, 31, 28, 31, 30, 31, 30, 31, 31,
-				30, 31, 30, 31 };
+				30, 31, 30, 31 }; // 계산의 편의를 위해 0번째에 0일 추가
 		hourOnOneDay = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-				13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 };
+				13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 }; // // 계산의 편의를 위해 0번째에 0일 추가
 	}
 
 	public int getNumberOfDaysOnYear(int startYear) {
 		if ((startYear % 4 == 0) && (startYear % 100 != 0)
 				|| startYear % 400 == 0) {
-			numberOfDaysOnYear = 366;
+			numberOfDaysOnYear = 366; // 윤년
 		} else {
 			numberOfDaysOnYear = 365;
 		}
@@ -24,7 +22,7 @@ public class dataForCalculate {
 	public int[] getNumberOfDaysOnMonth(int startYear) {
 		if ((startYear % 4 == 0) && (startYear % 100 != 0)
 				|| startYear % 400 == 0) {
-			numberOfDaysOnMonth[2] = 29;
+			numberOfDaysOnMonth[2] = 29; // 윤년 2월
 		} else {
 			numberOfDaysOnMonth[2] = 28;
 		}
